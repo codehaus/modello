b644a510280321dbb6a649ede7db4d64e63f12c3
dennisl <dennisl@f0865d36-f2f9-0310-a560-dfd520db29d7>
2008-01-15 21:48:51 +0000

    [maven-release-plugin]  copy for tag modello-1.0-alpha-18

---
+++ tags/modello-1.0-alpha-18/modello-maven-plugin/src/main/java/org/codehaus/modello/maven/ModelloXdocMojo.java
@@ -0,0 +1,103 @@
+package org.codehaus.modello.maven;
+
+/*
+ * Copyright (c) 2004, Codehaus.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.codehaus.modello.ModelloParameterConstants;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * Creates documentation for the model in xdoc format.
+ *
+ * @goal xdoc
+ *
+ * @author <a href="mailto:brett@apache.org">Brett Porter</a>
+ * @version $Id$
+ */
+public class ModelloXdocMojo
+    extends AbstractModelloGeneratorMojo
+{
+    /**
+     * The output directory of the generated documentation.
+     *
+     * @parameter expression="${basedir}/target/generated-site/xdoc"
+     *
+     * @required
+     */
+    private File outputDirectory;
+
+    /**
+     * The first version of the model. This is used to decide whether or not
+     * to show the since column. If this is not specified, it defaults to the
+     * version of the model, which in turn means that the since column will not
+     * be shown.
+     *
+     * @parameter
+     * @since 1.0-alpha-14
+     */
+    private String firstVersion;
+
+    protected String getGeneratorType()
+    {
+        return "xdoc";
+    }
+
+    protected boolean producesCompilableResult()
+    {
+        return false;
+    }
+
+    public File getOutputDirectory()
+    {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory( File outputDirectory )
+    {
+        this.outputDirectory = outputDirectory;
+    }
+
+    protected void customizeParameters( Properties parameters )
+    {
+        super.customizeParameters( parameters );
+
+        // Use version if firstVersion was not specified
+        if ( firstVersion == null )
+        {
+            firstVersion = getVersion();
+        }
+
+        parameters.put( ModelloParameterConstants.FIRST_VERSION, firstVersion );
+    }
+
+    public String getFirstVersion()
+    {
+        return firstVersion;
+    }
+
+    public void setFirstVersion( String firstVersion )
+    {
+        this.firstVersion = firstVersion;
+    }
+}
